671b90f1ac8b92fb8a9480fea27a9b572257c321
safuente <santiagoafu@ext.inditex.com>
2024-10-13 21:16:33 +0200

    First func

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,16 @@
+fn add_five(num: u32) -> u32 {
+   num + 5
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut x: u32 = 50;
+    println!("x is {}", x);
+    let y: u32 = add_five(x);
+    println!("y is {}", y);
+
+    x = 70;
+    println!("x is {}", x);
+
 }
