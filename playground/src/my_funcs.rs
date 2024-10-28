aad182fbd4ec79521f1c16fd57eb42101a7afeff
safuente <santiagoafu@ext.inditex.com>
2024-10-28 19:38:26 +0100

    Added doc

--- playground/src/my_funcs.rs
+++ playground/src/my_funcs.rs
@@ -1,5 +1,31 @@
+/// Function: add_five
+/// 
+/// # Arguments (num: u32)
+/// 
+/// # Example
+/// '''
+/// let x = 5;
+/// let y = add_five(x);
+/// '''
+/// 
+/**
+ * 
+ */
 pub fn add_five(num: u32) -> u32 {
-    num + 5
- 
- }
+    num + 5 // adds five
+}
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn add_five_test() {
+        let x: u32 = 100;
+        let y = add_five(x);
+
+        // print x and y
+        println!("x and y are from test {} {}", x, y);
+        assert_eq!(y, 105);
+    }
+}
--- playground/src/other_funcs/minus_funcs.rs
+++ playground/src/other_funcs/minus_funcs.rs
@@ -2,3 +2,17 @@ pub fn substract_10(num: u32) -> u32 {
     num - 10
  }
  
+
+
+ #[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn substract_10_test() {
+        let x: u32 = 100;
+        let y: u32 = substract_10(x);
+        println!("x and y are from test {} {}", x, y);
+        assert_eq!(y, 90);
+    }
+}
